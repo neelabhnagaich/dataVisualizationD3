a03ea0275dd990fb20261dc25f90c6e2ccf88408
neelabhnagaich <neelabhnagaich@gmail.com>
2022-04-28 10:29:33 +0530

    basic barchart complete

--- barchart/src/App.js
+++ barchart/src/App.js
@@ -3,6 +3,7 @@ import * as d3 from "d3"
 
 const width = 960;
 const height = 500;
+const margin = {top: 20, right: 20, bottom: 20, left: 200};
 
 const csvUrl = 'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
 
@@ -24,18 +25,21 @@ function App() {
     return <pre>Loading....</pre>
   }
 
-  console.log(data[0]);
+  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = width - margin.right - margin.left;
+ 
+
 
   const yScale = d3.scaleBand()
                   .domain(data.map(d => d.Country))
-                  .range([0,height])
+                  .range([0,innerHeight])
+                  .paddingInner(0.15)
   
   const xScale = d3.scaleLinear()
                   .domain([0, d3.max(data, d => d.Population )])
-                  .range([0,width])
+                  .range([0,innerWidth])
 
 
-  console.log(yScale.bandwidth());
 
   return (
     <svg width={width} height={height}>
@@ -46,15 +50,50 @@ function App() {
         width - widht of a bar is determine by linearScale as the max width possible
       
       */}
-      {data.map((d) => (
-        <rect
-        key={d.Country}
-       
-        y={yScale(d.Country)}
-        width={xScale(d.Population)} 
-        height={yScale.bandwidth()}
-        />
-      ))}
+      {/* with this we can move our graph and create space for our axis */}
+      <g transform={`translate(${margin.left},${margin.top})`}>
+        {/* this will generate axis lines
+          x1
+          y1
+          x2
+          y2
+
+          dy=0.71em  - is a kind of magic number that moves the text value a bit away from line
+        
+        */}
+        {xScale.ticks().map((tick) => (
+          <g key={tick} transform={`translate(${xScale(tick)},0)`}>
+            <line y2={innerHeight} stroke="black" />
+            <text
+              dy="0.71em"
+              y={innerHeight + 3}
+              style={{ textAnchor: "middle" }}
+            >
+              {tick}
+            </text>
+          </g>
+        ))}
+
+        {yScale.domain().map((tick) => (
+          <text
+            key={tick}
+            style={{ textAnchor: "end" }}
+            dy="0.32em"
+            x={-3}
+            y={yScale(tick) + yScale.bandwidth() / 2}
+          >
+            {tick}
+          </text>
+        ))}
+        {data.map((d) => (
+          <rect
+            key={d.Country}
+            y={yScale(d.Country)}
+            width={xScale(d.Population)}
+            height={yScale.bandwidth()}
+          />
+        ))}
+      </g>
     </svg>
   );
   
